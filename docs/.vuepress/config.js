b742547e2f92afefede101849a81a7711be9c0a7
Nor <51310674+mihyunLee@users.noreply.github.com>
2023-09-20 21:49:06 +0900

    ♻️ REFACTOR: getSidebarArr 함수 분리 및 var -> const, let 키워드로 변경

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,3 +1,5 @@
+const getSidebarArr = require("./getSidebarArr");
+
 module.exports = {
   title: "Today Nor Learned",
   description: "오늘보다 성장할 내일을 위한 기록",
@@ -49,67 +51,3 @@ module.exports = {
     ],
   ],
 };
-
-/** 카테고리 자동 추가 */
-function getSidebarArr() {
-  var fs = require("fs");
-  var docsPath = __dirname + "/../_posts";
-  var sidebarArr = [];
-  var EtcFilelist = [];
-  var filelist = fs.readdirSync(docsPath);
-  filelist.forEach(function (file) {
-    var stat = fs.lstatSync(docsPath + "/" + file);
-    if (stat.isDirectory()) {
-      // directory
-      // title is file, children is readdirSync
-      var docsFolderPath = docsPath + "/" + file;
-      var list = fs.readdirSync(docsFolderPath);
-      sidebarArr.push(makeSidebarObject(file, list));
-    } else {
-      // NOT directory
-      // title is '/' children is file
-      EtcFilelist.push(file);
-    }
-  });
-  sidebarArr.push(makeSidebarObject("", EtcFilelist));
-  return sidebarArr;
-}
-
-function makeSidebarObject(folder, mdfileList) {
-  var path = folder ? "/_posts/" + folder + "/" : "/_posts/";
-
-  mdfileList = aheadOfReadme(mdfileList);
-  var tmpMdfileList = [];
-  // remove .md, add Path
-  mdfileList.forEach(function (mdfile) {
-    if (mdfile.substr(-3) === ".md") {
-      mdfile = mdfile.slice(0, -3) === "README" ? "" : mdfile.slice(0, -3);
-      tmpMdfileList.push(path + mdfile);
-    }
-  });
-  mdfileList = tmpMdfileList;
-
-  // remove folder prefix number
-  if (folder) {
-    var dotIdx = folder.indexOf(".");
-    var title = Number(folder.substr(0, dotIdx))
-      ? folder.substr(dotIdx + 1)
-      : folder;
-  } else {
-    title = "ETC";
-  }
-
-  return {
-    title: title,
-    children: mdfileList,
-  };
-}
-
-function aheadOfReadme(arr) {
-  // ['1.test.md','README.md'] => ['README.md','1.test.md']
-  var readmeIdx = arr.indexOf("README.md");
-  if (readmeIdx > 0) {
-    arr.unshift(arr.splice(readmeIdx, 1)[0]);
-  }
-  return arr;
-}
---
+++ docs/.vuepress/getSidebarArr.js
@@ -0,0 +1,69 @@
+/** 카테고리 자동 추가 */
+function getSidebarArr() {
+  const fs = require("fs");
+  const docsPath = __dirname + "/../_posts";
+  const sidebarArr = [];
+  const EtcFilelist = [];
+  const filelist = fs.readdirSync(docsPath);
+
+  filelist.forEach(function (file) {
+    const stat = fs.lstatSync(docsPath + "/" + file);
+    if (stat.isDirectory()) {
+      // directory
+      // title is file, children is readdirSync
+      const docsFolderPath = docsPath + "/" + file;
+      const list = fs.readdirSync(docsFolderPath);
+      sidebarArr.push(makeSidebarObject(file, list));
+    } else {
+      // NOT directory
+      // title is '/' children is file
+      EtcFilelist.push(file);
+    }
+  });
+
+  sidebarArr.push(makeSidebarObject("", EtcFilelist));
+
+  return sidebarArr;
+}
+
+function makeSidebarObject(folder, mdfileList) {
+  const path = folder ? "/_posts/" + folder + "/" : "/_posts/";
+  const tmpMdfileList = [];
+
+  mdfileList = aheadOfReadme(mdfileList);
+  // remove .md, add Path
+  mdfileList.forEach(function (mdfile) {
+    if (mdfile.substr(-3) === ".md") {
+      mdfile = mdfile.slice(0, -3) === "README" ? "" : mdfile.slice(0, -3);
+      tmpMdfileList.push(path + mdfile);
+    }
+  });
+  mdfileList = tmpMdfileList;
+
+  // remove folder prefix number
+  let title = "";
+  if (folder) {
+    const dotIdx = folder.indexOf(".");
+    title = Number(folder.substr(0, dotIdx))
+      ? folder.substr(dotIdx + 1)
+      : folder;
+  } else {
+    title = "ETC";
+  }
+
+  return {
+    title: title,
+    children: mdfileList,
+  };
+}
+
+function aheadOfReadme(arr) {
+  // ['1.test.md','README.md'] => ['README.md','1.test.md']
+  const readmeIdx = arr.indexOf("README.md");
+  if (readmeIdx > 0) {
+    arr.unshift(arr.splice(readmeIdx, 1)[0]);
+  }
+  return arr;
+}
+
+module.exports = getSidebarArr;
